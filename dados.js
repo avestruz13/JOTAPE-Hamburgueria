0d55d3978a923500a966bb9f1603604d90eb4ab6
Jotape Hamburgueria <joaocolegioseta@gmail.com>
2024-11-07 16:35:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,414 @@
+var MENU = {
+    "acai-acompanhamento": [
+
+        {
+            "id": "batata",
+            "img": "./img/cardapio/acompanhamentos/batata.jpg",
+            "name": "Batata Frita 500g",
+            "dsc": "Creme de Açaí 700ML",
+            "isacrecimo": "false",
+            "price": 18.00
+        }, 
+         {
+            "id": "batatacheddar",
+            "img": "./img/cardapio/acompanhamentos/batata_cheddar.jpg",
+            "name": "Batata c/ Cheddar 500g",
+            "dsc": "Creme de Açaí 700ML",
+            "isacrecimo": "false",
+            "price": 25.00
+        },
+        {
+            "id": "nuggets",
+            "img": "./img/cardapio/acompanhamentos/nuggets.jpg",
+            "name": "Nuggets 500g ",
+            "dsc": "Creme de Açaí 700ML",
+            "isacrecimo": "false",
+            "price": 30.00
+        },
+        {
+            "id": "onion",
+            "img": "./img/cardapio/acompanhamentos/onion.jpg",
+            "name": "Onion rings 500g",
+            "dsc": "Creme de Açaí 700ML",
+            "isacrecimo": "false",
+            "price": 35.00
+        }
+
+
+
+
+
+
+    ],
+    
+    
+    
+    "acai-creme": [
+        {
+            "id": "cheddar-burguer",
+            "img": "./img/cardapio/creme/cheddar burguer.jpg",
+            "name": "Cheddar Burguer",
+            "dsc": "Creme de Açaí 300ML",
+            "isacrecimo": "true",
+            "price": 32.00
+      
+        },
+        {
+            "id": "chessburguer",
+            "img": "./img/cardapio/creme/chessburguer.jpg",
+            "name": "Chessburguer ",
+            "dsc": "Creme de Açaí 300ML",
+            "isacrecimo": "true",
+            "price": 28.00
+      
+        },
+        {
+            "id": "salada-burguer",
+            "img": "./img/cardapio/creme/salada burguer.jpg",
+            "name": "Salada Burguer",
+            "dsc": "Creme de Açaí 300ML",
+            "isacrecimo": "true",
+            "price": 35.00
+      
+        },
+        {
+            "id": "bacon-burguer",
+            "img": "./img/cardapio/creme/bacon burguer.jpg",
+            "name": "Bacon Burguer",
+            "dsc": "Creme de Açaí 300ML",
+            "isacrecimo": "true",
+            "price": 25.00
+      
+        }
+
+    ],
+    "acai-vitamina": [
+        {
+            "id": "milkshake",
+            "img": "./img/cardapio/vitamina/milkshake.jpeg",
+            "name": "Milk Shake 300ML",
+            "dsc": "Vitamina de Açai 300ML",
+            "price": 5.00
+        },
+        {
+            "id": "h2o",
+            "img": "./img/cardapio/vitamina/h2o.jpg",
+            "name": "H2O 500ML",
+            "dsc": "Vitamina de Açai 300ML",
+            "isbebida": "false",
+            "price": 15.00
+        },
+        {
+            "id": "coca-cola",
+            "img": "./img/cardapio/vitamina/cocacola.jpg",
+            "name": "Coca-Cola 350ML",
+            "dsc": "Vitamina de Açai 300ML",
+            "isbebida": "false",
+            "price": 8.00
+        },
+        {
+            "id": "suco-laranja",
+            "img": "./img/cardapio/vitamina/laranja.jpg",
+            "name": "Suco de Laranja 300ML",
+            "dsc": "Vitamina de Açai 300ML",
+            "isbebida": "false",
+            "price": 9.00
+        }
+       
+    ],
+    "milk-shake": [
+        {
+            "id": "brownie",
+            "img": "./img/cardapio/mikshake/brownie.jpg",
+            "name": "Brownie ",
+            "dsc": "Milk-Shake 300ML",
+            "isacrecimo": "false",
+            "price": 15.00
+        },
+        {
+            "id": "cookie",
+            "img": "./img/cardapio/mikshake/cookie.jpg",
+            "name": "Cookie c/ Chocolate",
+            "dsc": "Cookie com gotas de chocolate ",
+            "isacrecimo": "false",
+            "price": 10.00
+        },
+        {
+            "id": "minipudim",
+            "img": "./img/cardapio/mikshake/pudim.jpg",
+            "name": "Mini Pudim",
+            "dsc": "Milk-Shake 700ML",
+            "isacrecimo": "false",
+            "price": 20.00
+        },
+        {
+            "id": "sorvete",
+            "img": "./img/cardapio/mikshake/sorvete.jpg",
+            "name": "Sorvete",
+            "dsc": "Milk-Shake 700ML",
+            "isacrecimo": "false",
+            "price": 13.00
+        }
+       
+       
+       
+    ]
+}
+
+var ACRESCIMOS = {
+    "acrescimos-comum": [
+        {
+            "id": "alface",
+            "name": "Alface",
+            "price": 2.00
+        },
+        {
+            "id": "tomate",
+            "name": "Tomate",
+            "price": 2.00
+        },     
+        {
+            "id": "cebola",
+            "name": "Cebola",
+            "price": 2.00
+        },
+        {
+            "id": "queijo",
+            "name": "Queijo",
+            "price": 2.00
+        },
+        {
+            "id": "bacon",
+            "name": "Bacon",
+            "price": 2.00
+        },
+        {
+            "id": "mostarda",
+            "name": "Mostarda",
+            "price": 2.00
+        },
+        {
+            "id": "maionese",
+            "name": "Maionese",
+            "price": 2.00
+        },  
+        {
+            "id": "ketchup",
+            "name": "Ketchup",
+            "price": 2.00
+        },
+        {
+            "id": "picles",
+            "name": "Picles",
+            "price": 2.00
+        },
+       
+        
+    ],
+    "acrescimos-especiais": [
+        {
+            "id": "carne",
+            "name": "Carne",
+            "price": 5.00
+        },
+
+        {
+            "id": "cheddar",
+            "name": "Cheddar",
+            "price": 4.00
+        },
+        {
+            "id": "cebola-caramelizada",
+            "name": "Cebola Carameli...",
+            "price": 3.50
+        },
+        {
+            "id": "catupiry",
+            "name": "Catupiry",
+            "price": 4.00
+        },
+        {
+            "id": "ovo",
+            "name": "Ovo",
+            "price": 2.00
+        },
+        {
+            "id": "maionese-verde",
+            "name": "Maionese Verde",
+            "price": 2.50
+        },
+        {
+            "id": "chimichurri",
+            "name": "Chimichurri",
+            "price": 2.00
+        },
+      
+    ],
+    "acrescimos-simples-comum": [
+        {
+            "id": "sal",
+            "name": "sal",
+            "price": 5.00
+        },
+        {
+            "id": "oregano",
+            "name": "oregano",
+            "price": 5.00
+        },
+        {
+            "id": "lemon peper",
+            "name": "lemon peper",
+            "price": 5.00
+        },
+      
+    ],
+   
+}
+
+var MILK_SHAKE = {
+    "sorvetes": [
+        {
+            "id": "chocolate",
+            "name": "Chocolate",
+            "desc": "sorvete"
+        },
+        {
+            "id": "morango",
+            "name": "Morango",
+            "desc": "sorvete"
+        },     
+        {
+            "id": "coco",
+            "name": "Coco",
+            "desc": "sorvete"
+        },
+        {
+            "id": "sensacao",
+            "name": "Sensação",
+            "desc": "sorvete"
+        },
+        {
+            "id": "amendoim",
+            "name": "Amendoim",
+            "desc": "sorvete"
+        },
+        {
+            "id": "bom-bom",
+            "name": "Bom-Bom",
+            "desc": "sorvete"
+        },
+        {
+            "id": "chicletes",
+            "name": "Chicletes",
+            "desc": "sorvete"
+        },
+        {
+            "id": "ninho",
+            "name": "Ninho",
+            "desc": "sorvete"
+        },
+        {
+            "id": "talento",
+            "name": "Talento",
+            "desc": "sorvete"
+        },
+
+        {
+            "id": "laka",
+            "name": "Laka",
+            "desc": "sorvete"
+        },
+        
+        {
+            "id": "limao",
+            "name": "Limão",
+            "desc": "sorvete"
+        },
+
+    ],
+    "coberturas": [
+        {
+            "id": "chocolate",
+            "name": "Chocolate",
+            "desc": "cobertura"
+        },
+        {
+            "id": "morango",
+            "name": "Morango",
+            "desc": "cobertura"
+        },     
+        {
+            "id": "leite-condensado",
+            "name": "Leite Condensado",
+            "desc": "cobertura"
+        },
+        {
+            "id": "doce-de-leite",
+            "name": "Doce de Leite",
+            "desc": "cobertura"
+        },
+        {
+            "id": "caramelo",
+            "name": "Caramelo",
+            "desc": "cobertura"
+        },
+        {
+            "id": "maracuja",
+            "name": "Maracujá",
+            "desc": "cobertura"
+        },
+        {
+            "id": "uva",
+            "name": "Uva",
+            "desc": "cobertura"
+        },
+
+    ]
+
+}
+
+// Remover comentario em produção
+
+function recarregarScript() {
+    // Obtenha o elemento script existente
+    var scriptElement = document.getElementById('dados');
+    var scriptElement2  = document.getElementById('appId');
+
+    if (scriptElement) {
+        // Remova o script do DOM
+        scriptElement.parentNode.removeChild(scriptElement);
+
+        // Crie um novo elemento script
+        var novoScript = document.createElement('script');
+
+        // Atribua o mesmo ID para garantir que o novo script seja reconhecido
+        novoScript.id = 'dados';
+
+        // Atribua o caminho do arquivo ao novo script
+        novoScript.src = './js/dados.js';
+
+        // Adicione o novo script ao final do corpo (ou ao local desejado)
+        document.body.appendChild(novoScript);
+        
+    } else {
+        //console.error('Script não encontrado.');
+    }
+}
+
+// Armazene o estado do console
+var consoleAberto = false;
+
+// Verifique regularmente se o console está aberto
+setInterval(function() {
+    if (window.outerWidth - window.innerWidth > 200 || window.outerHeight - window.innerHeight > 200) {
+        carregarDadosNovamente();
+
+    }
+}, 5000);
+
+function carregarDadosNovamente() {
+    MEU_CARRINHO = [];
+    localStorage.setItem('meu_carrinho', JSON.stringify([MEU_CARRINHO]));
+    recarregarScript();
+    //console.log('O console foi aberto..');
+}
